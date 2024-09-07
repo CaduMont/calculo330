fbb6f118a667bf16dc614565b6f77aa3709208e1
Carlos Eduardo Soeiro Monteiro <94386820+cadu-monteiro@users.noreply.github.com>
2024-09-07 15:54:38 +0000

    v6

--- assets/js/code.js
+++ assets/js/code.js
@@ -2,18 +2,15 @@ function calcularMediaDeBags()
     {
         var quantdadePax = document.getElementById('valor1').value;
         var media3Dias = document.getElementById('valor2').value;
-        
         var calcularMedia = quantdadePax * media3Dias;
-        var arredonda1 = calcularMedia.toFixed(1);
+        var result1 = calcularMedia.toFixed(1);
         document.getElementById('info').innerHTML = (calcularMedia +' kg')
     }
     function calcularDiferencaDeFuel()
     {
         var fuelComandante = document.getElementById('fuel-coma').value;
         var fuelRecomendado = document.getElementById('fuel-rec').value;
-
         var diferencaFuel = fuelComandante - fuelRecomendado;
-        var arredonda2 = diferencaFuel.toFixed(1);
         document.getElementById('infoFuel').innerHTML = (diferencaFuel +' kg')
         
     }
